a0c9ba8381991570377fde25fa58f121d0eed5ca
M4Dd0x is here ! <77256586+m4dd0c@users.noreply.github.com>
2024-05-19 15:07:50 +0530

    Update scrapper.js

--- scrapper.js
+++ scrapper.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
-const defaultTimeout = 600000; //10 min
+const defaultTimeout = 600000; // 10 min
+const protocolTimeout = 1200000; // 20 min
 
 export const get_pdf = async (pdf_page) => {
   try {
@@ -14,6 +15,7 @@ export const get_pdf = async (pdf_page) => {
         process.env.NODE_ENV === "production"
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
+      protocolTimeout,
     });
     const page = await browser.newPage();
 
@@ -40,6 +42,7 @@ export const get_pdf = async (pdf_page) => {
     return documentUrl;
   } catch (error) {
     console.error("Error getting download link get_pdf_err", error.message);
+    return null; // Ensure to return a value even in case of an error
   }
 };
 
@@ -56,39 +59,43 @@ export const search_pdf = async (query) => {
         process.env.NODE_ENV === "production"
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
+      protocolTimeout,
     });
     const page = await browser.newPage();
 
     await page.goto(`${process.env.QUERY_URL}${query}`, {
       timeout: defaultTimeout,
     });
+    
+    // Additional logging to understand where it might be failing
+    console.log("Navigated to search page, waiting for selector .files-new ul");
+
     await page.waitForSelector(".files-new ul", { timeout: defaultTimeout });
+    
+    console.log("Selector .files-new ul found, extracting links");
+    
     const links = await page.$$eval(".file-right a[href*='.html']", (elems) =>
       elems.slice(0, 10).map((el) => el.href)
     );
 
     const titles = links.map((link) => {
-      // Extract the part of the URL after the last '/'
       const lastPart = link.substring(
         link.lastIndexOf("/") + 1,
         link.lastIndexOf("-")
       );
-
-      // Replace hyphens with spaces
       const titleWithSpaces = lastPart.replace(/-/g, " ");
-
-      // Capitalize the first letter of each word
       const title = titleWithSpaces.replace(/\b\w/g, (char) =>
         char.toUpperCase()
       );
-
       return title;
     });
+
     const array = buildArray(titles, links);
     await browser.close();
     return array;
   } catch (error) {
     console.error("Error searching pdf search_pdf_err", error.message);
+    return []; // Ensure to return a value even in case of an error
   }
 };
 
